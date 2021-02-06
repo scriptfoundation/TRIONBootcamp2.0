f1322a347e449f6f1dca1246f477dfed49a4fdfd
unknown <ifebronr@gmail.com>
2021-02-06 19:36:15 +0530

    Added the Landing Page

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let now =  new Date().getTime();
+const eventDate = new Date("Apr 10, 2021 00:00:00")
+let dateElement = document.getElementById('date');
+
+let oneDay = 1000 * 60 * 60 * 24
+var timeleft = eventDate - now;
+
+//CountdownTimer
+var getTimeInterval = setInterval(function() {
+
+    var now = new Date().getTime();
+    
+    timeleft = eventDate - now; 
+   
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+    dateElement.innerHTML = days
+   
+    if (timeleft < 0) {
+        clearInterval(getTimeInterval);
+    }
+    return days
+}, 1000);
+console.log(getTimeInterval);
